f86ba8ac21279ec48e3c615d8f40769930dc85fa
sunnie <35954879+sunnie1992@users.noreply.github.com>
2019-10-17 10:48:59 +0800

    Update env.staging.js

--- src/config/env.staging.js
+++ src/config/env.staging.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: '微信网页授权demo',
-  baseUrl: 'https://test.top1buyer.com',
+  baseUrl: 'https://test.xxx.com',
   api: {
     base_api: 'https://www.xxx.com/',
     common_api: 'https://www.xxx.com/common'
